9132eae1ba743f9344754df2ec069d1ed52fc157
elizabethharkins <elizabeth.lidstrom@gmail.com>
2022-07-11 16:13:48 -0400

    server updated to reflect routes

--- company-search__server/server.js
+++ company-search__server/server.js
@@ -2,13 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const companiesRouter = require("./routes/companies.js");
-const jobsRouter = require("./routes/jobs.js");
 const port = process.env.port || 8080;
 
 app.use(cors());
 app.use(express.json());
 app.use(companiesRouter);
-app.use(jobsRouter);
 
 app.get("/", (req, res, next) => {
   res.send("Welcome, you've got ... a server.");
